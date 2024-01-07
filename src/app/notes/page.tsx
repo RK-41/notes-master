e85e4e3e84fc65bffd108c713c91cf8adaa7a73c
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-01-07 23:28:00 +0530

    Initial commit
    Auth functionality using Clerk.
    Navbar.

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NotesMaster - Notes",
+};
+
+export default function NotesPage() {
+  return <div>Notes Area</div>;
+}
